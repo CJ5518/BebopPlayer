ba9029ec71b26f2e11589d9895f4a4fd085375c1
Carson Rueber <carsonrueber@gmail.com>
2021-08-21 23:12:15 -0700

    Update title to reflect version

--- Main.cs
+++ Main.cs
@@ -71,7 +71,7 @@ public class MainForm : Form {
 		
 		//Things for this window
 		this.Size = new Size(600, 175);
-		this.Text = "Bebop Music Player v3.3";
+		this.Text = "Bebop Music Player v3.3.1";
 		this.Icon = BebopPlayerV3.Properties.Resources.Icon;
 		
 		BackColor = Color.FromArgb(171, 171, 171);
